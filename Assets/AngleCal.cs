a0a710483ba2bbaa6b42c17b7103dee6edb6bad4
dswembou <56019569+dswembou@users.noreply.github.com>
2021-03-08 13:04:33 +0100

    Angle mathmetics tryout

---
+++ Assets/AngleCal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleCal : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calculate1();
+        Calculate2();
+    }
+
+    void Calculate1()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        print(targetDir);
+        Vector3 forward = transform.forward;
+
+        float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
+
+        print("Angle : " + angle);
+    }
+    
+    void Calculate2()
+    {
+        Vector3 targetDir = transform.position - target.position;
+        //print(targetDir);
+        Vector3 forward = target.forward;
+
+        float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
+
+        print("Angle : " + angle);
+    }
+}
